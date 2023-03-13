c35ba5158d88ff6821dd61885017eadb19082fd3
Colin van Loo <colin.vanloo@edu.tbz.ch>
2023-03-13 20:36:19 +0100

    extracted types header

--- str_with_len.c
+++ str_with_len.c
@@ -3,19 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <stdint.h>
-
-typedef uint8_t U8;
-typedef uint16_t U16;
-typedef uint32_t U32;
-typedef uint64_t U64;
-typedef int8_t S8;
-typedef int16_t S16;
-typedef int32_t S32;
-typedef int64_t S64;
-typedef float F32;
-typedef double F64;
-typedef unsigned char byte;
+#include "types.h"
 
 #define HEAD 1*sizeof(U32)
 #define SIZE(s) ((U32 *)(s))[-1]
---
+++ types.h
@@ -0,0 +1,18 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <stdint.h>
+
+typedef uint8_t U8;
+typedef uint16_t U16;
+typedef uint32_t U32;
+typedef uint64_t U64;
+typedef int8_t S8;
+typedef int16_t S16;
+typedef int32_t S32;
+typedef int64_t S64;
+typedef float F32;
+typedef double F64;
+typedef unsigned char byte;
+
+#endif /* TYPES_H */
